9c89fcc2111a605dde32801720acfd7390d44d3d
Zeki ÜNAL <zekiunal@gmail.com>
2014-02-24 08:37:17 +0200

    FirstCommit

---
+++ js/models/todo.js
@@ -0,0 +1,27 @@
+/*global Backbone */
+var app = app || {};
+
+(function () {
+	'use strict';
+
+	// Todo Model
+	// ----------
+
+	// Our basic **Todo** model has `title`, `order`, and `completed` attributes.
+	app.Todo = Backbone.Model.extend({
+		// Default attributes for the todo
+		// and ensure that each todo created has `title` and `completed` keys.
+		defaults: {
+			title: '',
+			completed: false
+		},
+        idAttribute: "todo_id",
+
+		// Toggle the `completed` state of this todo item.
+		toggle: function () {
+			this.save({
+				completed: Boolean(!this.get('completed'))
+			});
+		}
+	});
+})();
